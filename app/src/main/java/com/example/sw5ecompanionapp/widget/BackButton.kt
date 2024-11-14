89fb92f127884c3ed957f4429c7d7427e7f64cf0
Lefteris Chatzismalis <146983163+LefChatz@users.noreply.github.com>
2024-11-14 13:24:52 +0200

    debug

--- app/src/main/java/com/example/sw5ecompanionapp/widget/BackButton.kt
+++ app/src/main/java/com/example/sw5ecompanionapp/widget/BackButton.kt
@@ -4,9 +4,7 @@ import android.content.Context
 import android.content.res.TypedArray
 import android.util.AttributeSet
 import android.view.View
-import android.widget.ImageView
 import android.widget.RelativeLayout
-import android.widget.TextView
 import com.example.sw5ecompanionapp.R
 
 class BackButton @JvmOverloads constructor(
@@ -15,7 +13,7 @@ class BackButton @JvmOverloads constructor(
     defStyleAttr: Int = 0
 ) : RelativeLayout(context, attrs, defStyleAttr) {
     private val attributes: TypedArray = context.obtainStyledAttributes(attrs, R.styleable.BackButton)
-    private val view: View = View.inflate(context, R.layout.back_button, this)
+    private val view: View = View.inflate(context, R.layout.universal_back_button, this)
 
     init {
         attributes.recycle()
